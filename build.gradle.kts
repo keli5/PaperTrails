c85a2146446bb0dbe58e2a80f19ff482ea8af0c9
keli5 <46039037+keli5@users.noreply.github.com>
2022-10-29 19:40:22 -0500

    Fix incompatible version of serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.10"
     id("xyz.jpenilla.run-paper") version "1.0.6"
 }
 
