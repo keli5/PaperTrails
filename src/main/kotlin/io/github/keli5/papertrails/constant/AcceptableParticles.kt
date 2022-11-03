185e7d7781f10ae3a64af8793cc8bee64d584e0e
keli5 <46039037+keli5@users.noreply.github.com>
2022-11-02 20:51:52 -0500

    Prepare for listmodes and listparticles

--- src/main/kotlin/io/github/keli5/papertrails/constant/AcceptableParticles.kt
+++ src/main/kotlin/io/github/keli5/papertrails/constant/AcceptableParticles.kt
@@ -3,6 +3,8 @@ package io.github.keli5.papertrails.constant
 import org.bukkit.Particle
 
 val acceptableParticles = mapOf(
+    "" to null,
+    null to null,
     "cloud" to Particle.CLOUD,
     "waxon" to Particle.WAX_ON,
 )
--- src/main/kotlin/io/github/keli5/papertrails/constant/ParticleModes.kt
+++ src/main/kotlin/io/github/keli5/papertrails/constant/ParticleModes.kt
@@ -1,32 +1,40 @@
-package io.github.keli5.papertrails
+package io.github.keli5.papertrails.constant
 
 import org.bukkit.Location
 import org.bukkit.World
+import java.util.Calendar
+import kotlin.math.sin
 
 fun aboveHead(world: World, x: Double, y: Double, z: Double): Location {
     return Location(world, x, y+2, z)
 }
+fun aboveHeadBounce(world: World, x: Double, y: Double, z: Double): Location {
+    return Location(world, x,
+                 y + (sin(Calendar.getInstance().timeInMillis.toDouble() / 1000) * 0.5) + 0.5 + 2,
+                    z)  // time divided by 1000 for more continuous sin output
+}
+
 
 enum class ParticleMode(val modeName: String) {
-    ABOVEHEAD("abovehead");
+    ABOVEHEAD("abovehead"),
+    ABOVEHEAD_BOUNCE("aboveheadbounce");
 
     companion object {
-        infix fun from(value: String): ParticleMode? = ParticleMode.values().firstOrNull { it.modeName == value }
+        infix fun from(value: String?): ParticleMode? = ParticleMode.values().firstOrNull { it.modeName == value }
     }
 }
 
 val ParticleModes = mapOf(
-    ParticleMode.ABOVEHEAD to ::aboveHead
+    ParticleMode.ABOVEHEAD to ::aboveHead,
+    ParticleMode.ABOVEHEAD_BOUNCE to ::aboveHeadBounce
 )
 
 fun particlePosition(mode: ParticleMode, world: World, x: Double, y: Double, z: Double): Location {
     return ParticleModes[mode]!!(world, x, y, z)
 }
-
 fun particlePosition(mode: ParticleMode, location: Location): Location {
     return ParticleModes[mode]!!(location.world, location.x, location.y, location.z)
 }
-
 fun particleMode(modeName: String): ParticleMode {
     var mode = ParticleMode from modeName
     if (mode == null) mode = ParticleMode.ABOVEHEAD
